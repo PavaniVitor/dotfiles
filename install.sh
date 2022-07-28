2189049560bb8db4c370d1e0f6ea2bfdaa0cffa1
Vitor Pavani <vitoralvespavani@gmail.com>
2022-07-28 16:42:48 -0300

    Make stow location aware.

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 
 FOLDERS="bash,tmux,alacritty,nvim,git"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 for folder in $(echo $FOLDERS | sed "s/,/ /g")
 do
-    stow -D $folder
-    stow $folder
+    stow -D --target="$HOME" --dir="$SCRIPT_DIR" $folder
+    stow -S --target="$HOME" --dir="$SCRIPT_DIR" $folder
 done
 
