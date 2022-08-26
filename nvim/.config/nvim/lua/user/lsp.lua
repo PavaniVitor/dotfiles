d86dd6e3743a1d9be585eeaa55b1a26f5eb9ef88
Vitor Pavani <vitoralvespavani@gmail.com>
2022-08-26 00:10:43 -0300

    setup mason-lspconfig.

--- nvim/.config/nvim/lua/user/lsp.lua
+++ nvim/.config/nvim/lua/user/lsp.lua
@@ -1,6 +1,7 @@
 
 
 require("mason").setup()
+require("mason-lspconfig").setup()
 require("mason-lspconfig").setup_handlers({
   function (server_name) -- default handler (optional)
     require("lspconfig")[server_name].setup {}
