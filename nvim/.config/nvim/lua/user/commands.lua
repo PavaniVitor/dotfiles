47c0de33b33f9445b13187da8185e1da2e874134
Vitor Pavani <vitoralvespavani@gmail.com>
2024-02-27 10:36:03 -0300

    fix invalid commands

--- nvim/.config/nvim/lua/user/commands.lua
+++ nvim/.config/nvim/lua/user/commands.lua
@@ -1,10 +1,7 @@
 local cmd = vim.api.nvim_create_user_command
 
-cmd('WQ', 'wq', {})
-cmd('Wq', 'wq', {})
 cmd('W', 'w', {})
-cmd('Q', 'q', {})
-cmd('WQ!', 'wq', {bang = true})
-cmd('Wq!', 'wq', {bang = true})
-cmd('Q', 'q!', {bang = true})
+cmd('WQ', 'wq', {bang = true})
+cmd('Wq', 'wq', {bang = true})
+cmd('Q', 'q', {bang=true})
 
